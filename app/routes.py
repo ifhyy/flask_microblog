8c35be63c4dee4aaa4bc4fb381d4bf56a69e0ce6
stupk <bozzya7@gmail.com>
2024-01-07 10:19:31 +0100

    feat: login form and templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,29 @@
+from flask import render_template, request, redirect, flash, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'bozzy'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Logged in successfully")
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
