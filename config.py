25217c87c78d5136d019b1a25dc32b2a2f8276e7
stupk <bozzya7@gmail.com>
2024-01-15 12:39:44 +0100

    feat: user and post models

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+import dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv.load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                   'sqlite:///' + os.path.join(basedir, 'app.db')
